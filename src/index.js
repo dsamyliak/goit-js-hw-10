d33eeebfaa19313353f273dbaf48c371bbc99e47
Dmytro Samyliak <dsamyliak@gmail.com>
2021-11-08 16:05:32 +0200

    Update cw-10

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './css/styles.css';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+var debounce = require('lodash.debounce');
+
+const DEBOUNCE_DELAY = 300;
+
+
+debounce(consoleInfo, 5000, { 'leading': true, 'trailing': false, });
+
+function consoleInfo() {
+ console.log("XXXhhhXXXhhh")
+};
+
+console.log(on.debounce);
